d27802e376be864f38d9b4930b75640ae9c0e8e8
Luke Cutting <lukecutting123@gmail.com>
2023-04-27 15:35:16 -0400

    Minor changes to degree format

--- src/data/resume/degrees.js
+++ src/data/resume/degrees.js
@@ -1,7 +1,7 @@
 const degrees = [
   {
     school: 'University of Florida',
-    degree: 'B.S. Business Administration, Computer Science',
+    degree: 'Business Administration, Computer Science',
     link: 'https://ufl.edu',
     year: 2023,
   },
