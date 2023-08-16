c95a29d363af2aee3d1b0f15c3434782e93957cc
Luke Cutting <lukecutting123@gmail.com>
2023-08-16 10:22:23 -0500

    HTML and CSS cert

--- src/data/certifications.js
+++ src/data/certifications.js
@@ -8,7 +8,17 @@ const data = [
       date: '2023-08-06',
       desc:
         'This is my certification I received from Meta for completing their React Basics course. ' +
-        'This course taught me the basics of React and how to use it to create a website.',
+        'This course taught me the basics of React and how to use it to create a web app.',
+    },
+    {
+      title: 'HTML and CSS in Depth',
+      subtitle: 'Website',
+      link: 'https://www.coursera.org/account/accomplishments/verify/72CZZQ4HL9MC',
+      image: '/images/certifications/HTML&CSS_Cert.png',
+      date: '2023-08-16',
+      desc:
+        'This is my certification I received from Meta for completing their HTML and CSS in depth course. ' +
+        'This course took a deep dive into the web development languages HTML and CSS. I learned how to be proficient in both languages and how to use them to create a website.',
     },
     {
       title: 'Introduction to Front-End Development',
